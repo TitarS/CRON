66d3ca1fce95e410ba66fa21f390261ccaf18a2b
Serhii Tytar <serhii.tytar@gmail.com>
2017-06-19 15:49:31 +0300

    2

--- cron.sh
+++ cron.sh
@@ -11,3 +11,8 @@ find $where_find -name "*.tar.gz" -type f -mtime +$day -delete
 #find $where_find -type f -empty
 # Найти пустые папки
 find $where_find -type d -empty -exec rmdir -pv '{}' \; 2>/dev/nullr
+
+#Для Open Server
+%progdir%\modules\wget\bin\wget.exe -q --no-cache http://timefile/tht.php
+#или
+%progdir%\modules\php\%phpdriver%\php-win.exe -c %progdir%\userdata\temp\config\php.ini -q -f %sitedir%\timefile\cron.php
